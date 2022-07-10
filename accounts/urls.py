b479dd2cbf3c7f7e194158ab516c82f3082d8c20
pg-endokenta <k.endo@4nonome.com>
2022-07-10 20:23:05 +0900

    Add WelcomeView

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 
 from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 urlpatterns = [
-    # path('', views.WelcomeView.as_view(), name='welcome'),
+    path("", views.WelcomeView.as_view(), name="welcome"),
     # path('signup/', views.SignUpView.as_view(), name='signup'),
     # path('home/', views.HomeView.as_view(), name='home'),
     # path('', include('django.contrib.auth.urls')),
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class WelcomeView(TemplateView):
+    template_name = "welcome/index.html"
