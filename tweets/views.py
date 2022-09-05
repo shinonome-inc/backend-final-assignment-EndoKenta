c7fb22891fc18617c644a5ebdb7cd61b9cca3bb1
pg-endokenta <k.endo@4nonome.com>
2022-09-05 13:39:04 +0900

    ツイート作成、詳細、削除機能の追加

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,42 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import Http404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView
+
+from .models import Tweet
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(LoginRequiredMixin, ListView):
     template_name = "tweets/home.html"
+    queryset = Tweet.objects.all().select_related("user").order_by("-created_at")
+    context_object_name = "tweets"
+
+
+class TweetCreateView(LoginRequiredMixin, CreateView):
+    model = Tweet
+    fields = ["content"]
+    template_name = "tweets/create.html"
+    success_url = reverse_lazy("tweets:home")
+
+    def form_valid(self, form):
+        form.instance.user_id = self.request.user.id
+        return super().form_valid(form)
+
+
+class TweetDetailView(LoginRequiredMixin, DetailView):
+    model = Tweet
+    template_name = "tweets/detail.html"
+
+
+class TweetDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "tweets/delete.html"
+    model = Tweet
+    success_url = reverse_lazy("tweets:home")
+
+    def test_func(self):
+        if Tweet.objects.filter(pk=self.kwargs["pk"]).exists():
+            current_user = self.request.user
+            tweet_user = Tweet.objects.get(pk=self.kwargs["pk"]).user
+            return current_user.pk == tweet_user.pk
+        else:
+            raise Http404
