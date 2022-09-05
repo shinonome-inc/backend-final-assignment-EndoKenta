7602f40939337df436f0fd55114586e4c30b22b3
pg-endokenta <k.endo@4nonome.com>
2022-09-05 13:31:07 +0900

    tweetのモデルを作成

--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
-# class Tweet(models.Model):
-#     pass
+class Tweet(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=140)
+    created_at = models.DateTimeField(default=timezone.now)
