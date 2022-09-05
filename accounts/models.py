bc6f981e1abc18e988d9707e675a5dc31531e747
pg-endokenta <k.endo@4nonome.com>
2022-09-05 13:52:09 +0900

    UserProfileの作成、URLにusenameを使うことからusernameを英数字のみに制限、それに伴いマイグレーションファイルの再生成

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,25 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        validators=[RegexValidator(r"^[a-zA-Z0-9]*$", "使用できるのは半角アルファベット、半角数字のみです")],
+        help_text="この項目は必須です。半角アルファベット、半角数字で150文字以下にしてください。",
+        verbose_name="ユーザー名",
+    )
     email = models.EmailField(max_length=254)
+    slug = models.SlugField(max_length=150, blank=False, unique=True)
+
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.username)
+        return super().save(*args, **kwargs)
+
 
 # class FriendShip(models.Model):
 #     pass
--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,11 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     # path('home/', views.HomeView.as_view(), name='home'),
     # path('', include('django.contrib.auth.urls')),
-    # path('profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path(
+        "profile/<slug:slug>",
+        views.UserProfileView.as_view(),
+        name="user_profile",
+    ),
     # path('profile/edit/', views.UserProfileEditView.as_view(), name='user_profile_edit'),
     # path('<str:username>/following_list/', views.FollowingListView.as_view(), name='following_list'),
     # path('<str:username>/follower_list/', views.FollowerListView.as_view(), name='follower_list'),
