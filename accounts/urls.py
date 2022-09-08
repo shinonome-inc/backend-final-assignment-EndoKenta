af9ced231a9472e7ceb229161a423c8db4060a94
pg-endokenta <k.endo@4nonome.com>
2022-09-08 21:38:06 +0900

    urlの追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -18,8 +18,16 @@ urlpatterns = [
         name="user_profile",
     ),
     # path('profile/edit/', views.UserProfileEditView.as_view(), name='user_profile_edit'),
-    # path('<str:username>/following_list/', views.FollowingListView.as_view(), name='following_list'),
-    # path('<str:username>/follower_list/', views.FollowerListView.as_view(), name='follower_list'),
-    # path('<str:username>/follow/', views.FollowView.as_view(), name='follow'),
-    # path('<str:username>/unfollow/', views.UnFollowView, name='unfollow'),
+    path(
+        "<str:username>/following_list/",
+        views.FollowingListView.as_view(),
+        name="following_list",
+    ),
+    path(
+        "<str:username>/follower_list/",
+        views.FollowerListView.as_view(),
+        name="follower_list",
+    ),
+    path("<str:username>/follow/", views.FollowView, name="follow"),
+    path("<str:username>/unfollow/", views.UnFollowView, name="unfollow"),
 ]
